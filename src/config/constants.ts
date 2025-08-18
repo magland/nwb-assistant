7955066a199d7e99294eea453f91a638146b8d05
Ben Dichter <ben.dichter@gmail.com>
2025-08-18 12:24:40 -0400

    Add deployment link button to header alongside GitHub link

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,4 @@
 // Configuration constants for the NWB Assistant application
 export const GITHUB_REPO_URL = 'https://github.com/magland/nwb-assistant';
 export const GITHUB_ISSUES_URL = `${GITHUB_REPO_URL}/issues`;
+export const DEPLOYMENT_URL = 'https://magland.github.io/nwb-assistant/chat';
